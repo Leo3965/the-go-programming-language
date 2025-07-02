6476fee15e705530f2c81be946ee834f2afe45d4
leonarolivei <leonardo.noliveira@mercadolivre.com>
2025-07-02 18:25:31 -0300

    feat: print every argument in a new line

--- ch1/echo2.go
+++ ch1/echo2.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
+	exercise()
+}
+
+func example() {
 	s, sep := "", " "
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 	}
 	fmt.Println(s)
+}
+
+func exercise() {
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index, arg)
+	}
 }
